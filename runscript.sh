465279d5725b3ea89b723c6a856c0e9dbf6b1381
James Hodgkinson <james@terminaloutcomes.com>
2021-03-26 21:26:00 +1000

    updated

--- runscript.sh
+++ runscript.sh
@@ -2,7 +2,11 @@
 
 cd "$HOME/hangnail-remover/" || exit 1
 
-git pull
+git -C "$HOME/hangnail-remover/" reset --hard
+git -C "$HOME/hangnail-remover/" pull
+git -C "$HOME/hangnail-remover/sourcerepo/" reset --hard
+git -C "$HOME/hangnail-remover/sourcerepo/" pull
+
 python3 hangnail_inspector.py --update
 
 git add hangnail_data.json
